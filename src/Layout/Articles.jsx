53f99333aff01e9b9f46076d59ff510e639fc5bb
Luis Fernando <fernu292@gmail.com>
2022-01-06 11:10:33 -0600

    Finalizando Blogs

--- src/Layout/Articles.jsx
+++ src/Layout/Articles.jsx
@@ -4,6 +4,8 @@ import Article from "../components/Article";
 
 import Currency from "../images/image-currency.jpg";
 import Restaurant from "../images/image-restaurant.jpg";
+import Plane from "../images/image-plane.jpg";
+import Confetti from "../images/image-confetti.jpg";
 
 const articles = [
   {
@@ -19,6 +21,20 @@ const articles = [
     content: `Our simple budgeting feature allows you to separate out your spending and set 
     realistic limits each month. That means you …`,
     img: `${Restaurant}`,
+  },
+  {
+    title: 'Take your Easybank card wherever you go',
+    author: 'By Wilson Hutton',
+    content: `We want you to enjoy your travels. This is why we don’t charge any fees on purchases 
+    while you’re abroad. We’ll even show you …`,
+    img: `${Plane}`
+  }, 
+  {
+    title: 'Our invite-only Beta accounts are now live!',
+    author: 'By Claire Robinson',
+    content: `After a lot of hard work by the whole team, we’re excited to launch our closed beta. 
+    It’s easy to request an invite through the site ...`,
+    img:`${Confetti}`
   }
 ];
 
